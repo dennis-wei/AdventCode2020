5f79bb8bacdeb1b49ac8cfcbfa5a7fe729738931
Dennis Wei <dw2654@columbia.edu>
2020-12-08 21:15:56 -0800

    day 9 (966, 642)

---
+++ 9/run.py
@@ -0,0 +1,71 @@
+
+import os, sys
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+import argparse
+import clipboard
+
+from itertools import combinations
+from collections import defaultdict, Counter, deque
+from util.helpers import split_newline, space_split, int_parsed_list, list_of_ints, get_all_nums, submit
+from math import floor, ceil
+from functools import reduce
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--from-std-in", action='store_true', default=False)
+args = parser.parse_args()
+if args.from_std_in:
+    raw_input = clipboard.paste()
+else:
+    with open("input.txt", "r") as f:
+        raw_input = f.read()
+raw_input = raw_input.strip()
+
+input = raw_input
+
+def create_sliding_window(input, start, stop):
+    return set(input[start:stop])
+
+def pair_exists(target, window):
+    for e in window:
+        if target - e in window:
+            return True
+    return False
+
+
+WINDOW_SIZE = 25
+def solve(input):
+    part1 = None
+    nums = [int(n.strip()) for n in input.split("\n")]
+    for i in range(WINDOW_SIZE, len(nums)):
+        window = create_sliding_window(nums, i - WINDOW_SIZE, i)
+        target = nums[i]
+        if not pair_exists(target, window):
+            part1 = target
+            break
+
+    start = 0
+    stop = 1
+    while True:
+        sum_attempt = sum(nums[start:stop])
+        if sum_attempt == part1:
+            break
+        elif sum_attempt < part1:
+            stop += 1
+        elif sum_attempt > part1:
+            start += 1
+    
+    min_num = min(nums[start:stop])
+    max_num = max(nums[start:stop])
+
+    return part1, min_num + max_num
+
+answer1, answer2 = solve(input)
+
+
+print("Part 1")
+print(f"Answer: {answer1}")
+# print(submit(9, 1, answer1).text)
+
+print("Part 2")
+print(f"Answer: {answer2}")
+# print(submit(9, 2, answer1).text)
